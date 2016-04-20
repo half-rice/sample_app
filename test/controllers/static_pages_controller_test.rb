9c95d3ae90009c71729d7a3ad4ef5bd0de01720a
surrus <xsurrus@gmail.com>
2016-04-20 03:32:40 -0600

    added and linked about page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | the motherfucking homepage"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | for panzies who can't figure shit out"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | it's what we're about"
   end
 
 end
